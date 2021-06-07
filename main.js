56a3baeec58ceca2c248a363aebf2b840e228861
danainjax <danainjax@gmail.com>
2021-06-07 13:38:48 -0400

    hearts and likes added, working on the mimic server call

--- main.js
+++ main.js
@@ -1,8 +1,30 @@
 // Defining text characters for the empty and full hearts for you to use later.
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
-
+// When a user clicks on an empty heart:
+//   * Invoke `mimicServerCall` to simulate making a server request
+//   * When the "server" returns a failure status:
+//     * Respond to the error using a `.catch(() => {})` block after your
+//       `.then(() => {})` block.
+//     * Display the error modal by removing the `.hidden` class
+//     * Display the server error message in the modal
+//     * Use `setTimeout` to hide the modal after 3 seconds (add the `.hidden` class)
 // Your JavaScript code goes here!
+const likes = document.querySelectorAll('.like');
+console.log(likes);
+likes.forEach(heart =>{
+  heart.addEventListener('click', (e) => {
+    if (e.target.innerHTML === EMPTY_HEART) {
+      e.target.innerHTML = FULL_HEART;
+      e.target.outerHTML = `<span class="activated-heart">${FULL_HEART}</span>`
+    }else 
+     e.target.innerHTML = EMPTY_HEART
+     e.target.outerHTML = `<span class="like-glyph">${EMPTY_HEART}</span>`
+    
+    
+    
+  })
+})
 
 
 
